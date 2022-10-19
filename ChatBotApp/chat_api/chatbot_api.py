d517e70e8131cf5379ff61ccb1c02475fecf4900
LemonPepperSeasoning <takahiro.swe@gmail.com>
2022-10-19 16:56:46 +1300

    Cleaned up UI & move OpenAI api key into dotenv file

--- ChatBotApp/chat_api/chatbot_api.py
+++ ChatBotApp/chat_api/chatbot_api.py
@@ -1,11 +1,9 @@
 import os
 import openai
 
-API_KEY = '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'
-
 
 def chat(message):
-    openai.api_key = API_KEY
+    openai.api_key = os.environ.get("OPEN_AI_API_KEY")
     completion = openai.Completion.create(engine="curie", prompt = message,
     temperature=0.4, stop=['\nHuman'], top_p=1, frequency_penalty=0,presence_penalty=0.1,best_of=1)
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,12 @@
 import React, {useState} from 'react'
 
-import { Link } from "react-router-dom";
-
 const Home = ( ) => {
-
-    return (
     
-    <>
-        <main>
-          <h2>Welcome to the homepage!</h2>
-        </main>
-
-        <nav>
-          <Link to="/vid2vid">Vid2Vid</Link>
-        </nav>
-
-        <nav>
-          <Link to="/img2vid">Img2Vid</Link>
-        </nav>
-      </>
+  
+    return (
+      <div>
+        Home
+      </div>
     )
 }
 
